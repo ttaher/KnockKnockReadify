1162657aedb87f7e281e94259313b78d3d53afea
ttaher <talat.t.mohammed@gmail.com>
2018-12-01 13:25:03 +0300

    enhance 88%

--- KnockKnockTests/Controllers/ValuesControllerTests.cs
+++ KnockKnockTests/Controllers/ValuesControllerTests.cs
@@ -80,13 +80,3 @@ namespace KnockKnock.Controllers.Tests
         }
     }
 }
-
-///api/values/ReverseWords sentence = trailing % 20space%20&X-ARR-LOG-ID=f873bfd9-45f3-45bf-aa0f-ef7314654795 443 - 65.52.168.84 - ARRAffinity=06fb770771bc781fdba780d4ae2dee40ec646d4fe38b09509f95b08402cf2fec - talattestknock.azurewebsites.net 200 0 0 461 1013 15
-///api/values/ReverseWords sentence = Capital & X - ARR - LOG - ID = e0f2c58c - 57a3-4f88-a4e1-53506d2fdb68 443 - 65.52.168.84 - ARRAffinity=06fb770771bc781fdba780d4ae2dee40ec646d4fe38b09509f95b08402cf2fec - talattestknock.azurewebsites.net 200 0 0 452 977 15
-///api/values/ReverseWords sentence = Bang % 21 & X - ARR - LOG - ID = 831a6909-6924-4b30-89ed-9b11b2de4339 443 - 65.52.168.84 - ARRAffinity=06fb770771bc781fdba780d4ae2dee40ec646d4fe38b09509f95b08402cf2fec - talattestknock.azurewebsites.net 200 0 0 450 977 15
-///api/values/ReverseWords sentence =% C2 % BFQu % C3 % A9 % 3F & X - ARR - LOG - ID = b02cfc88 - ed08 - 4ff9-8323-77861dfd1174 443 - 65.52.168.84 - ARRAffinity=06fb770771bc781fdba780d4ae2dee40ec646d4fe38b09509f95b08402cf2fec - talattestknock.azurewebsites.net 200 0 0 452 1007 15
-///api/values/ReverseWords sentence = detartrated % 20kayak%20detartrated&X-ARR-LOG-ID=aad58c83-6572-4878-9747-04217c6b2c3b 443 - 65.52.168.84 - ARRAffinity=06fb770771bc781fdba780d4ae2dee40ec646d4fe38b09509f95b08402cf2fec - talattestknock.azurewebsites.net 200 0 0 475 1055 15
-///api/values/ReverseWords sentence = This % 20 is% 20a%20snark:%20%E2%B8%AE&X-ARR-LOG-ID=4a46a276-4e29-4df7-81b2-b244007ad3e0 443 - 65.52.168.84 - ARRAffinity=06fb770771bc781fdba780d4ae2dee40ec646d4fe38b09509f95b08402cf2fec - talattestknock.azurewebsites.net 200 0 0 466 1058 0
-///api/values/ReverseWords sentence =% 20 % 20S%20%20P%20%20A%20%20C%20%20E%20%20Y%20%20&X-ARR-LOG-ID=6777e3bd-365c-4435-b1a6-074747f9bdef 443 - 65.52.168.84 - ARRAffinity=06fb770771bc781fdba780d4ae2dee40ec646d4fe38b09509f95b08402cf2fec - talattestknock.azurewebsites.net 200 0 0 466 1100 15
-///api/values/ReverseWords sentence =% 21B%21A%21N%21G%21S%21&X-ARR-LOG-ID=b6c6a121-9468-44c7-a85c-61fb11cafec3 443 - 65.52.168.84 - ARRAffinity=06fb770771bc781fdba780d4ae2dee40ec646d4fe38b09509f95b08402cf2fec - talattestknock.azurewebsites.net 200 0 0 457 1025 31
-///api/values/ReverseWords sentence = P % 21u % 40n%23c%24t%25u%5Ea%26t%2Ai%28o%29n&X-ARR-LOG-ID=aeeec8bf-4197-4d3d-b9c6-ca81cfc00e10 443 - 65.52.168.84 - ARRAffinity=06fb770771bc781fdba780d4ae2dee40ec646d4fe38b09509f95b08402cf2fec - talattestknock.azurewebsites.net 200 0 0 467 1079 15
